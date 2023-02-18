e070e9be8029962ac1340569cb142f43065da83f
Rachel Spencer <rachelspencer688@gmail.com>
2023-02-18 17:36:14 -0500

    submitHandlers implemented

---
+++ src/components/Auth.js
@@ -0,0 +1,89 @@
+import {useState} from 'react'
+import axios from 'axios'
+ 
+const Auth = () => {
+   const [username, setUsername] = useState('')
+   const [password, setPassword] = useState('')
+   const [register, setRegister] = useState(true)
+
+   const handleUsername = (event) => {
+    setUsername(event.target.value)
+   };
+
+   const handlePassword = (event) => {
+    setPassword(event.target.value)
+   };
+
+   const handleClick = (event) => {
+    setRegister(!register)
+   };
+ 
+   const submitHandler = async (e) => {
+       e.preventDefault()
+       let response;
+
+       const body = {
+            username,
+            password
+        }
+  
+        try {
+            if (register) {
+                response = await axios.post('https://socialmtn.devmountain.com/register', body);
+            } else {
+                response = axios.post('https://socialmtn.devmountain.com/login', body);
+            } 
+        } catch (error) {
+            console.log(error);
+            setUsername('')
+            setPassword('')
+        }
+       console.log('response', response);
+   }
+
+        // const body = {
+        //     username,
+        //     password
+        // }
+
+        // const baseUrl = 'https://socialmtn.devmountain.com';
+        // const endpoint = register ? 'register' : 'login';
+
+        // try {
+        //     response = await axios.post(`${baseUrl}/${endpoint}`, body);
+        // } catch (error) {
+        //     console.log(error);
+        //     setUsername('')
+        //     setPassword('')
+        // }
+        // console.log('response', response);
+        // }
+   
+   return (
+       <main>
+           <h1>Welcome!</h1>
+           <form className="form auth-form" onSubmit={submitHandler}>
+               <input
+                   className="form-input"
+                   type= "text"
+                   placeholder= "Username"
+                   value= {username}
+                   onChange={handleUsername}
+                />
+               <input
+                   className="form-input"
+                   type= "text"
+                   placeholder= "Password"
+                   value= {password}
+                   onChange={handlePassword}
+                />
+               <button className="form-btn">
+                   {register ? "Sign Up" : "Login"}
+               </button>
+           </form>
+           <button onClick={handleClick} className='form-btn'>Need to {register ? "Login" : "Sign Up"}?</button>
+       </main>
+   )
+}
+ 
+export default Auth
